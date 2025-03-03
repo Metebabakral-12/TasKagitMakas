a7806d46ec35d58e8bc21142e3a52ebc63ac3624
Metebabakral-12 <simsekmete7777@gmail.com>
2025-03-03 18:19:57 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+let ㅤㅤㅤㅤㅤㅤelㅤㅤㅤㅤㅤㅤ = 0
+input.onGesture(Gesture.Shake, function () {
+    ㅤㅤㅤㅤㅤㅤelㅤㅤㅤㅤㅤㅤ = randint(0, 2)
+    if (ㅤㅤㅤㅤㅤㅤelㅤㅤㅤㅤㅤㅤ == 0) {
+    	
+    } else if (ㅤㅤㅤㅤㅤㅤelㅤㅤㅤㅤㅤㅤ == 1) {
+    	
+    } else if (ㅤㅤㅤㅤㅤㅤelㅤㅤㅤㅤㅤㅤ == 2) {
+    	
+    }
+})
